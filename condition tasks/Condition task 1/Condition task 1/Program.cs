8999301a7141b54fe6e60a78b281af3150c40585
LauriSuokas <lauriv.suokas@gmail.com>
2018-09-19 09:07:24 +0300

    condition tasks added

---
+++ condition tasks/Condition task 1/Condition task 1/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace Condition_task_1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Ohjelma selvittää onko annettu luku positiivinen, negatiivinen vai nolla");
+            Console.WriteLine("Syötä numero")
+        }
+    }
+}
