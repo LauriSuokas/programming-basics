52fbcc70a05d6d1afc7d620375c5e5f071002c38
LauriSuokas <lauriv.suokas@gmail.com>
2018-09-20 08:32:58 +0300

    conditon task 2 initialized

---
+++ condition tasks/condition task 2/condition task 2/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace condition_task_2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
