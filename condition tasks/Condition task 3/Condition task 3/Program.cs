c6b40638e784bf0804a835e8c811365ae3ebb18f
LauriSuokas <lauriv.suokas@gmail.com>
2018-09-20 09:43:45 +0300

    condition task 3 first part added

--- condition tasks/Condition task 3/Condition task 3/Program.cs
+++ condition tasks/Condition task 3/Condition task 3/Program.cs
@@ -6,7 +6,25 @@ namespace Condition_task_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Ohjelma selvittää onko luku negatiivinen vai positiivinen ja onko luku pariton vai parillinen");
+            Console.Write("Syötä numero: ");
+            string userInput = Console.ReadLine();
+            int number = 0;
+            bool isNumber = int.TryParse(userInput, out number);
+
+            if (isNumber)
+            {
+                if (number == 0)
+                    Console.WriteLine("Luku on nolla! ");
+
+                else if (number < 0)
+                    Console.WriteLine($"Numero {number} on negatiivinen");
+
+                else
+                        Console.WriteLine($"Numero {number} on positiivinen");
+            }
+
+            
         }
     }
 }
