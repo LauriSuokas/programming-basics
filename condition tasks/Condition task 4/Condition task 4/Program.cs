f3f01d9d23df72f5720e4834ea4a1982da1483ce
LauriSuokas <lauriv.suokas@gmail.com>
2018-09-25 09:08:32 +0300

    condition task initilized

---
+++ condition tasks/Condition task 4/Condition task 4/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Condition_task_4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
