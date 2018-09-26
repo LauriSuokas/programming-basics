25569cdbf8d701894900b4b6b9612af724554aff
LauriSuokas <lauriv.suokas@gmail.com>
2018-09-26 12:00:03 +0300

    condition task 4 updated

--- condition tasks/Condition task 4/Condition task 4/Program.cs
+++ condition tasks/Condition task 4/Condition task 4/Program.cs
@@ -7,7 +7,31 @@ namespace Condition_task_4
         static void Main(string[] args)
         {
             Console.WriteLine("Ohjelma lajittelee luvut nousevaan järjestykseen");
-            Console.WriteLine("Syötä numero");
+            Console.WriteLine("Syötä 1. luku: ");
+            string userInput = Console.ReadLine();
+
+            int number = int.Parse(userInput);
+            Console.Write("Syötä 1. luku");
+
+            int number1 = int.Parse(userInput);
+            Console.Write("Syötä 2. luku");
+
+            int number2 = int.Parse(userInput);
+            Console.Write("Syötä 3. luku");
+
+            if (number < number1)
+            {
+                if (number1 < number2)
+                    Console.WriteLine($"Luku {number}, {number1}, {number2}");
+
+                else
+                {
+                    Console.WriteLine($"Luku {number}, {number2}, {number1}");
+                    if (number1 < number)
+                        Console.WriteLine($"Luku {number1}, {number}, {number2}");
+                }
+
+            }
         }
     }
 }
