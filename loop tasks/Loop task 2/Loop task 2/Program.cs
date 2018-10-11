033fe5f6bf19fa0c04456c0c7ec391003f46f614
LauriSuokas <lauriv.suokas@gmail.com>
2018-10-11 13:37:48 +0300

    loop tas 2 done

--- loop tasks/Loop task 2/Loop task 2/Program.cs
+++ loop tasks/Loop task 2/Loop task 2/Program.cs
@@ -6,7 +6,17 @@ namespace Loop_task_2
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Program solves factorials");
+            int number = 0;
+            int i = 1;
+            int summ = 0;
+            Console.WriteLine("Input number");
+            number = int.Parse(Console.ReadLine());
+            for (i = 1; i <= number; i++)
+            {
+                summ = summ + i;
+            }
+            Console.WriteLine($"Summ is {summ}");
         }
     }
 }
