4d61203b7ffe844f3985b67fcc335451566f4583
LauriSuokas <lauriv.suokas@gmail.com>
2018-10-11 13:56:12 +0300

    loop task 3 done

--- loop tasks/Loop task 3/Loop task 3/Program.cs
+++ loop tasks/Loop task 3/Loop task 3/Program.cs
@@ -6,7 +6,29 @@ namespace Loop_task_3
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("Program solves odd and even numbers sum");
+            int number = 0;
+            int i = 0;
+            int evenSum = 0;
+            int oddSum = 0;
+            do
+            {
+                Console.WriteLine("Input number");
+                number = int.Parse(Console.ReadLine());
+                if (number <= 0)
+                    Console.WriteLine("Wrong input");
+            }
+            while (number <= 0);
+
+            for (i = 0; i <= number; i++)
+            {
+                if (i % 2 == 0)
+                    evenSum = evenSum + i;
+                else
+                    oddSum = oddSum + i;
+            }
+            Console.WriteLine($"Sum of even numbers is {evenSum}");
+            Console.WriteLine($"Sum of odd numbers is {oddSum}");
         }
     }
 }
